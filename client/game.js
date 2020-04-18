41b8c0f2291f8d7a515b993222b08655fb8757c6
Theo Reichert <theo.reichert7@gmail.com>
2020-04-18 19:57:02 +0200

    18.04.2020

---
+++ client/game.js
@@ -0,0 +1,47 @@
+const socket = io();
+
+
+
+//Login------------------------------------------------------------------------
+$('#login form').submit(function (e) {
+    e.preventDefault(); // prevents default action of e/the button so page reloading
+    socket.emit('Login', $('#login form #loginName').val());
+    $('#login').slideUp();
+    return false;
+});
+//END Login--------------------------------------------------------------------
+
+//All around the chat----------------------------------------------------------
+$('.chat > button').click(function () {
+    $('.info .chat .window').slideToggle("fast", "swing");
+});
+
+$('.chat .window > form').submit(function (e) {
+    e.preventDefault(); // prevents page reloading
+    socket.emit('MessageFromClient', PlayerObject.name + ": " + $('.chat .window form #message').val());
+    $('.chat .window form #message').val('');
+    return false;
+});
+
+var chatWindowsList = $('.chat .window > ul');
+socket.on('MessageFromServer', function (message) {
+    chatWindowsList.prepend($('<li>').text(message));
+});
+
+//END All around the chat------------------------------------------------------
+
+
+
+//------------------------------------------------------
+
+var infoName = $('.wrapper .info #Name');
+var infoPoints = $('.wrapper .info #Points');
+var infoGuesses = $('.wrapper .info #Guesses');
+socket.on('PlayerObject', (JSON_PlayerObject) => {
+    PlayerObject = JSON.parse(JSON_PlayerObject);
+    infoName.append(PlayerObject.name);
+    infoPoints.append(PlayerObject.points);
+    infoGuesses.append(PlayerObject.guesses);
+});
+//END--------------------------------------------------
+
