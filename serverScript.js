f76352123b1d8dcace94ea7781f51397adad6ea9
Theo Reichert <theo.reichert7@gmail.com>
2020-12-28 02:08:21 +0100

    ordered everything; game.start, next up game.round, especially card.distribute

--- serverScript.js
+++ serverScript.js
@@ -5,26 +5,81 @@
 var IDs = [0, 0, 0, 0, 0, 0];
 var ID;
 var playerList = [];
-function Player(name, socket_id) {
-    for (let i = 0; i < IDs.length; i++) {
-        if (IDs[i] == 0) {
-            IDs[i] = 1;
-            ID = i;
-            break;
+class Player {
+    constructor (name, socket_id) {
+        for (let i = 0; i < IDs.length; i++) {
+            if (IDs[i] == 0) {
+                IDs[i] = 1;
+                ID = i;
+                break;
+            }
         }
+        this.name = name;
+        this.id = ID;
+        this.socket_id = socket_id;
+        this.points = 0;
+        this.guesses = 0;
+        this.cards = [];
+        console.log(this);
     }
-    this.name = name;
-    this.id = ID;
-    this.socket_id = socket_id;
-    this.points = 0;
-    this.guesses = 0;
-    this.cards = [];
-    console.log(this);
 }
-//END Cards Generator------------------------------------------------------
+//END Player Generator------------------------------------------------------
 var already_voted = [];
 
+var playingfield = {
+    cards: [],
+    playingstack: [],
+    shuffle: () => {
+        for (i = this.cards.length - 1; i > 0; i--) {
+            j = Math.floor(Math.random() * i)
+            let k = this.cards[i]
+            this.cards[i] = this.cards[j]
+            this.cards[j] = k
+        }
+        console.log(playingfield.cards);
+    }
+}
 
+//Cards Generator----------------------------------------------------------
+class Card {
+    constructor(color, value) {
+    this.color = color;
+    this.value = value;
+    }
+}
+
+//global set of cards
+//var cards = [];
+var cardIndex = 0;
+var colors = ["red", "green", "blue", "yellow"]
+
+for (color of colors) {
+    for (x = 1; x < 14; x++) {
+        playingfield.cards[cardIndex] = new Card(color, x);
+        cardIndex++;
+    }
+}
+for (x = 1; x < 5; x++) {
+    playingfield.cards[cardIndex] = new Card(undefined, 14); //Zauberer
+    cardIndex++;
+}
+for (x = 1; x < 5; x++) {
+    playingfield.cards[cardIndex] = new Card(undefined, 0); //Narren
+    cardIndex++;
+}
+//END Card Generator-------------------------------------------------------
+
+
+
+//Schuffle playingfield.cards-----------------------------------------------------------
+function shuffleCards() {
+    
+}
+//END Schuffle playingfield.cards-------------------------------------------------------
+
+
+
+/*
 function login (name, socket_id) {
     playerList.push(new Player(name, socket_id));
     console.log("New Player " + playerList[playerList.length - 1].name + " logged in.");
@@ -39,11 +94,67 @@ function vote (playerid) {
         socket.emit('vote.update', already_voted.length, playerList.length);
         if (already_voted.length == playerList.length) {
             //START GAME
-            socket.emit('prepare_game');
-            console.log("START GAME");
+            console.log("game.start");
+            socket.emit('game.start');
+            
         }
     }
 }
+
+async function game() {
+    let ready = await ready;
+        io.emit('game.start');
+        console.log("START GAME");
+        const AmountOfRounds = playingfield.cards.lenght() / playerList.length;
+        console.log("The amount of rounds to be played is " + AmountOfRounds.toString());
+        var trumpColor;
+        for (round = 1; round <= AmountOfRounds; round++) {
+            //determine trump color
+            trumpColor = colors[Math.floor(Math.random() * 4)];
+            //
+            io.emit('game.round', round, trumpColor);
+            //io.emit('MessageFromServer', "Trump is " + trumpColor + ". " + trumpColor + " is trump.");
+            shuffleCards();
+            cardIndex = 0;
+            for (let id = 0; id < playerList.length; id++) {
+                playerList[id].playingfield.cards = playingfield.cards.slice(cardIndex, round);
+                cardIndex = cardIndex + round;
+                for (let i = 0; i < playerList.lenght; i++) {
+                    if (playerList[i].socket_id == socket.id) {
+
+                    }
+                }
+                socket.emit('playingfield.cards.hand.update', playerList);
+            }
+    }
+}
+
+
+function game() {
+    const AmountOfRounds = playingfield.cards.lenght() / playerList.length;
+    console.log("The amount of rounds to be played is " + AmountOfRounds.toString());
+    var trumpColor;
+    for (round = 1; round <= AmountOfRounds; round++) {
+        //determine trump color
+        trumpColor = colors[Math.floor(Math.random() * 4)];
+        //
+        io.emit('game.round', round, trumpColor);
+        //io.emit('MessageFromServer', "Trump is " + trumpColor + ". " + trumpColor + " is trump.");
+        shuffleCards();
+        cardIndex = 0;
+        for (let id = 0; id < playerList.length; id++) {
+            playerList[id].playingfield.cards = playingfield.cards.slice(cardIndex, round);
+            cardIndex = cardIndex + round;
+            for (let i = 0; i < playerList.lenght; i++) {
+                if (playerList[i].socket_id == socket.id) {
+
+                }
+            }
+            socket.emit('playingfield.cards.hand.update', playerList);
+        }
+    }
+}
+*/
 
 //Server Setup-------------------------------------------------------------
 const express = require('express');
@@ -54,32 +165,42 @@ const IPaddress = '192.168.178.32'; //enter your current ip address inorder to a
 const port = 80;
 //-------------------------------------------------------------------------
 io.on('connection', function (socket) { //parameter of the callbackfunction here called 'socket' is the connection to the client that connected 
+    console.log(Object.keys(io.sockets.sockets));
+    console.log(Object.keys(io.sockets.connected));
     console.log('a user connected');
     socket.on('toServerConsole', function (text) { console.log(text); });
     socket.on('login', (name) => {
         if (playerList.length < 6) {
             playerList.push(new Player(name, socket.id));
+            console.log("login.successful");
+            socket.emit('login.successful', JSON.stringify(playerList[playerList.length - 1]));
             console.log("IDs: " + IDs)
             console.log("New Player " + playerList[playerList.length - 1].name + " logged in.");
-            socket.emit('login.successful', JSON.stringify(playerList[playerList.length - 1]));
-            io.emit('vote.update', already_voted.length, playerList.length);
             io.emit('MessageFromServer', playerList[playerList.length - 1].name + " logged in.");
+            io.emit('vote.update', already_voted.length, playerList.length);
         } else {
+            console.log("login.unsuccessful");
             socket.emit('login.unsuccessful');
         }
     });
     socket.on('MessageFromClient', (message) => { io.emit('MessageFromServer', message); });
     socket.on('vote', (playerid) => {
+        console.log("vote");
         if (!already_voted.includes(playerid)) {
-            console.log("vote");
+            console.log("vote accepted");
             already_voted.push(playerid);
             io.emit('vote.update', already_voted.length, playerList.length);
             if (already_voted.length == playerList.length) {
-                //START GAME
-                io.emit('prepare_game');
-                console.log("START GAME");
+                console.log("start game");
+                io.emit('game.start');
+                var round = 1;
             }
-        }
+        } else { console.log("vote rejected"); }
+    });
+    socket.on('card.play', (color, number) => {
+        console.log("card.play");
+        playingfield.cardplayed(color, number);
+        io.emit('card.update', color, number);
     });
     socket.on('disconnect', (reason) => {
         console.log('user disconnected');
@@ -95,7 +216,8 @@ io.on('connection', function (socket) { //parameter of the callbackfunction here
         }
         console.log("IDs: " + IDs)
     });
-});           
+});
+
 app.use(express.static('client'));
 app.get('/', function (req, res) {
     if (playerList.length < 6) {
@@ -108,70 +230,9 @@ httpsserver.listen(port, IPaddress, function () {
     console.log( 'Server is listening on ' + IPaddress + ':' + port.toString() );
 });
 //END Server Setup---------------------------------------------------------
-
-
-
-//Cards Generator----------------------------------------------------------
-function card(color, value) {
-    this.color = color;
-    this.value = value;
-}
-//global set of cards
-var cards = [];
-var cardIndex = 0;
-var colors = ["red", "green", "blue", "yellow"]
-
-for (color of colors) {
-    for (x = 1; x < 14; x++) {
-        cards[cardIndex] = new card(color, x);
-        cardIndex++;
-    }
-}
-for (x = 1; x < 5; x++) {
-    cards[cardIndex] = new card(undefined, 14);
-    cardIndex++;
-}
-for (x = 1; x < 5; x++) {
-    cards[cardIndex] = new card(undefined, 0);
-    cardIndex++;
-}
-//END Card Generator-------------------------------------------------------
-
-
-
-//Schuffle Cards-----------------------------------------------------------
-function shuffleCards() {
-    for (i = cards.length - 1; i > 0; i--) {
-        j = Math.floor(Math.random() * i)
-        k = cards[i]
-        cards[i] = cards[j]
-        cards[j] = k
-    }
-    console.log(cards);
-}
-//END Schuffle Cards-------------------------------------------------------
-
-
+console.log(Object.keys(io.sockets.sockets));
+console.log(Object.keys(io.sockets.connected));
 
 //GAME---------------------------------------------------------------------
-function game() {
-    const AmountOfRounds = cards.lenght() / playerList.length;
-    console.log("The amount of rounds to be played is " + AmountOfRounds.toString());
-    var trumpColor;
-    for (round = 1; round <= AmountOfRounds; round++) {
-        //determine trump color
-        trumpColor = colors[Math.floor(Math.random() * 4)];
-        //
-        io.emit('newRound', round, trumpColor); 
-        //io.emit('MessageFromServer', "Trump is " + trumpColor + ". " + trumpColor + " is trump.");
-        shuffleCards();
-        cardIndex = 0;
-        for (id = 0; id < playerList.length; id++) {
-            playerList[id].cards = cards.slice(cardIndex, cardIndex + round); //this is copying the elements of the cards array with the index 'cardIndex' to 'cardIndex + round -1' into the playerList object's cards array
-            cardIndex = cardIndex + round;
-        }
-        //now send the playerList[id].card arrays to the associated clients / sockets
-
-    }
-}
+io.emit('MessageFromServer', "Ya know I am ready.");
 //END GAME-----------------------------------------------------------------
