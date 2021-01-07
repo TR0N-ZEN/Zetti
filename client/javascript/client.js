920e865ff28b35b0ad99aa4c394ad5a972fd3a98
Theo <theo.reichert7@gmail.com>
2021-01-07 02:19:37 +0100

    fixed points_update()

--- client/javascript/client.js
+++ client/javascript/client.js
@@ -1,36 +1,48 @@
 const socket = io();
-var chat_hidden = false;
-var chatWindowsList = $('.chat .window > ul');
+var chat = {
+    hidden: false,
+    window: $('.info .chat .window'),
+    WindowList: $('.chat .window > ul')
+};
 var playerBoard = $('.playerBoard');
-var infoName = $('.wrapper .info #Name');
-var infoRound = $('.wrapper .info #Round');
-var infoTrump = $('.wrapper .info #Trump');
-var infoPoints = $('.wrapper .info #Points');
-var infoGuesses = $('.wrapper .info #Guesses');
 var hand = $('.wrapper .hand');
 var cards = $('.wrapper .hand div');
 var playingStack = $('.playingStack');
 
+var info = {
+    name: $('.wrapper .info #Name'),
+    round: $('.wrapper .info #Round'),
+    trump: $('.wrapper .info #Trump'),
+    points: $('.wrapper .info #Points'),
+    guesses: $('.wrapper .info #Guesses'),
+    chat: $('.info .chat')
+};
+var guesses = {
+    take: $('.take_guess'),
+    hide: () => {
+        let width_in_px = guesses.take.css('width');
+        guesses.take.css("right", "-" + width_in_px);
+    }
+};
 
 $('.chat > button').click( () => {
     //$('.info .chat .window').slideToggle("fast", "swing");
-    if (chat_hidden) {
-        $('.info .chat .window').css('right', 0);
-        chat_hidden = false;
+    if (chat.hidden) {
+        chat.window.css('right', 0);
+        chat.hidden = false;
     } else {
         let width_in_px = $('.info .chat .window').css('width');
-        let width = "-" + width_in_px;
-        $('.info .chat .window').css('right', width);
-        chat_hidden = true;
+        chat.window.css('right', "-" + width_in_px);
+        chat.hidden = true;
     }
 });
 
 
 
 //EMITTER------------------------------------------------------------------------
-/* 
+/*
  * socket.emit(x, ...)
- * 
+ *
  * login
  * MessageFromClient
  * vote
@@ -54,14 +66,18 @@ $('.chat .window > form').submit(function (button) {
 $('#ready_player > button').on('click', () => {
     socket.emit('vote', PlayerObject.id);
 });
-$('.take_guess > form').submit( function (button) {
+$('.take_guess > form').submit(function (button) {
     button.preventDefault();
+    guesses.hide();
     let guess_number = $('.take_guess > form > input').val();
+    $('.take_guess > form > input').val("");
     socket.emit('guess.response', guess_number, index);
-    $('.info > div > #Guesses').text('Guesses: ' + guess_number);
-    let width_in_px = $('.take_guess').css('width');
-    let width = "-" + width_in_px;
-    $('.take_guess').css("righ", width);
+    info.guesses.text('Guesses: ' + guess_number);
+    let width_in_px = guesses.take.css('width');
+    guesses.take.css("righ", "-" + width_in_px);
+    setTimeout(() => {
+        guesses.take.css("display", "grid");
+    }, 1000);
 });
 
 
@@ -69,7 +85,7 @@ $('.take_guess > form').submit( function (button) {
 //LISTENERS---------------------------------------------------
 /*
  * socket.on(x, function)
- * 
+ *
  * login.
  *      successful
  *      unsuccessful
@@ -94,15 +110,15 @@ $('.take_guess > form').submit( function (button) {
  * points.
  *      update
  * changeCSS
- * */ 
+ * */
 
  var index = 0;
 socket.on('login.successful', (JSON_PlayerObject) => {
     $('#login').slideUp();
     PlayerObject = JSON.parse(JSON_PlayerObject);
-    infoName.append(PlayerObject.name);
-    infoPoints.append(PlayerObject.points);
-    infoGuesses.append(PlayerObject.guesses);
+    info.name.append(PlayerObject.name);
+    info.points.append(PlayerObject.points);
+    info.guesses.append(PlayerObject.guesses);
     index = PlayerObject.index;
 });
 socket.on('login.unsuccessful', () => {
@@ -119,7 +135,7 @@ socket.on('vote.update', (votes, amount_of_players) => {
     $('#votes').text(votes.toString() + " / " + amount_of_players.toString());
 });
 socket.on('MessageFromServer', (message) => {
-    chatWindowsList.prepend($('<li>').text(message));
+    chat.WindowList.prepend($('<li>').text(message));
 });
 socket.on('game.start', () => {
     console.log("game.start");
@@ -128,8 +144,9 @@ socket.on('game.start', () => {
 });
 socket.on('game.round', (round, trumpColor) => {
     console.log("game.round :" + round.toString());
-    infoRound.text("Round: " + round.toString());
-    infoTrump.text("Trump: " + trumpColor.toString());
+    info.round.text("Round: " + round.toString());
+    info.trump.text("Trump: " + trumpColor.toString());
+    playingStack.html("");
 });
 socket.on('game.trick', () => {
     console.log("game.trick");
@@ -140,13 +157,12 @@ socket.on('guess.waitingFor', (playerName) => {
 })
 socket.on('guess.request', () => {
     let width_in_px = $('.take_guess').css('width');
-    let width = width_in_px;
-    $('.take_guess').css("right", width);
+    guesses.take.css("righ", "-" + width_in_px);
+    guesses.take.css("display", "grid");
+    guesses.take.css("right", width_in_px);
 });
 socket.on('guess.complete', () => {
-    let width_in_px = $('.take_guess').css('width');
-    let width = "-" + width_in_px;
-    $('.take_guess').css("right", width);
+    guesses.hide();
 });
 socket.on('card.distribute', (JSON_cards) => {
     console.log("card.distribute");
@@ -171,6 +187,7 @@ socket.on('card.waiting', () => {
         let card = $(this)[0].className.split("_");//.target.attributes.class.name;
         console.log("You clicked: " + card[0], card[1]);
         socket.emit('card.toPlayingstack', card[0], card[1]);
+        $(this).css("display", "none");
     });
 });
 socket.on('card.update', (color, number) => {
@@ -180,7 +197,7 @@ socket.on('card.update', (color, number) => {
     playingStack.html(card_svg);
 });
 socket.on('points.update', (points) => {
-    infoPoints.text("Points: " + points.toString());
+    info.points.text("Points: " + points.toString());
 });
 
 
