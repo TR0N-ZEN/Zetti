3cac613a9634279a0b2bd578385f7f7ccb74fa37
Theo Reichert <theo.reichert7@gmail.com>
2021-05-22 11:01:52 +0100

    untested branchless code for getID(Ids)

--- cpp/server_assets/client.cpp
+++ cpp/server_assets/client.cpp
@@ -2,13 +2,24 @@
 
 short int Client::getID(std::vector<short int>& Ids)
 {
-	int id = Ids.size();
-	for (int i = 0; i < Ids.size(); i++)
+	int id = 0;
+	if (Ids[0] != 0)
 	{
-		id = (Ids[i]==0)*(i<id)*i;
+		// for vectors with not many elements - 100% branchless but iterates all elements
+		for (int i = 1; i < Ids.size(); i++)
+		{
+			id += (Ids[i]==0)*(i<id)*i;
+		}
+		
+		// //for vectors with many elements - many if statements because of do-while-loop but doesn't iterate all elements
+		// 	int i = 1;
+		// 	do { id = (Ids[i] == 0)*(i); i++;} while (id == 0);
+		
+		// assuring id is different from 0
+		id += (id == 0)*(-1);
 	}
-	Ids[id] = 1;
-	return id;
+	if (id != (-1)) { Ids[id] = 1; }
+	return id; // returns -1 if all entries in Ids are different from 0
 };
 
 Client::Client(/*socket; */ std::vector<short int>& Ids)
