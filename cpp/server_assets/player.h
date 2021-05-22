8d12459bb34252621b6c41eed57a67ac2f8ec6f3
Theo Reichert <theo.reichert7@gmail.com>
2021-05-22 14:38:32 +0100

    minorities with datatypes

--- cpp/server_assets/player.h
+++ cpp/server_assets/player.h
@@ -6,14 +6,12 @@
 class Player : Client
 {
 	public:
-		short unsigned int id;
 		std::string name;
 		short int points;
 		short unsigned int guess;
 		short unsigned int tricks_won;
 		std::vector<Card*> hand;
-		static short int getID(std::vector<short unsigned int>& Ids);
-		static short int update_points(Player& player);
+		static void update_points(Player& player);
 		Player(/*socket, */std::vector<short unsigned int>& Ids, std::string Name, short int Points = 0, short unsigned int Guess = 0, short unsigned int Tricks_won = 0, std::vector<Card*> Hand = {});
 		~Player();
 #ifdef DEBUG
